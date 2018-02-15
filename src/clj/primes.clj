5ce606617551a92bdfaf422f44376f24837ad226
mihhailko <mihhail@kattenpad.nl>
2018-02-15 13:54:56 +0100

    some clj functions

---
+++ src/clj/primes.clj
@@ -0,0 +1,20 @@
+(ns primes)
+
+(use 'clojure.walk)
+
+(def primes {:this "this" :is "is" :something "something"})
+
+(def syntax '(:subject :predicate))
+
+(def dictionary {:oneword "oneword" :twoword "twoword" :threeword "threeword"})
+
+(defn word [dictentry primentry syntax] {syntax dictentry :meaning primentry})
+
+(keywordize-keys (word (:oneword dictionary)(:something primes):subject)) ;;probably unnecessary
+
+(def oneword (word (:oneword dictionary)(:something primes):subject))
+
+(defn meaning [word] (:meaning word))
+
+(meaning oneword)
+
