ca476c5c401c3d5dcd83c6649fc7887b67fff566
mihhailko <mihhail@kattenpad.nl>
2018-02-15 14:12:04 +0100

    more clj functions added

--- src/clj/primes.clj
+++ src/clj/primes.clj
@@ -18,3 +18,22 @@
 
 (meaning oneword)
 
+(defn role [word] (nth (keys word) 0))
+
+(role oneword)
+
+(def twoword (word (:twoword dictionary)[(:is primes)(:something primes)] :predicate))
+
+(defn print-sentence [word1 word2]
+  (if (and (:subject word1) (:predicate word2))
+    (println (:subject word1) (:predicate word2))
+    (println (:subject word2) (:predicate word1))))
+
+(defn meaning-sentence [word1 word2]
+  (if (and (:subject word1) (:predicate word2))
+    [(:meaning word1)(:meaning word2)]
+    [(:meaning word2)(:meaning word1)]))
+
+(meaning-sentence) oneword twoword
+
+
