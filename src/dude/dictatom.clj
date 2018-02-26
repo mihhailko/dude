86c81d568d93601d405f2b9a509b7eda0257e904
mihhailko <mihhail@kattenpad.nl>
2018-02-26 19:05:54 +0100

    "to-dudespeak fn"

--- src/dude/dictatom.clj
+++ src/dude/dictatom.clj
@@ -35,13 +35,6 @@
                   ((nth (keys @dict) n) @dict)
                   (recur tar (inc n)))))
 
-
-(defn translate [form tar]
-  (let [f (findtarm form 0)
-        l (findtarl tar 0)
-        p (findtarp (nth (:primes f) 0) l 0)]
-                p))
-
 (defn translate2 [form tar dict]
   (let [primes2  (first (filter #(= (:form %) form) (vals dict)))
         primes   (some #(if (= (:form %) form) (:primes %)) (vals dict))
@@ -66,49 +59,7 @@
 (to-dudespeak '("words" "are" "cool") @dict)
 
 
-;; to match over several primes in random order
 
-(defn findtarm [tar n dict]
-  (let [map  ((nth (keys dict) n) dict)
-        form (:form map)
-        cnt  (count (keys dict))]
-                (if (= tar form)
-                  map
-                  (recur tar (inc n) dict))))
-
-(defn translate3 [form tar dict]
-  (let [primes2  (first (filter #(= (:form %) form) (vals dict)))
-        primes   (some #(if (= (:form %) form) (:primes %)) (vals dict))
-        tar-form (some #(if
-                            (and
-                             (= (:primes %) primes)
-                             (= (:lang %) tar))
-                          (:form %)) (vals dict))]
-    tar-form))
-
-(translate3 "words" "ee" @dict)
-
-(filter #(= (:primes %) '("say" "many")) (vals @dict))
-
-(filter #(= (:primes %) '("say" "many")) (vals @dict))
-
-(some #(if (= (:form %) "words") (:primes %)) (vals @dict))
-
-(some #(if (= (:primes %) '("many" "say")) (:form %)) (vals @dict))
-
-(defn findtarp [tar map n]
-  (let [p (nth (:primes map) n)]
-                (if (= tar p)
-                  (:form map)
-                  (recur tar map (inc n)))))
-
-(some (partial = "say") '("many" "say"))
-
-(findtarm "words" 0 @dict)
-
-(def srcmap (find-list-primes "words" @dict))
-
-(def tarmap (find-list-primes "sõnad" @dict))
 
 
 
